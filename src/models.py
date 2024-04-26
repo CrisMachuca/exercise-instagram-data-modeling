3f85f401bfbf5c32d191d1f4e2eb4b6ca9da6e31
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-04-26 19:10:19 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -7,12 +7,72 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    userId = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario)
+    
+
+class Media(Base):
+    __tablename__ = 'Media'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    URL = Column(String(250), nullable=False)
+    postId = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, primary_key=True)
+    user_to_id = Column(String(250), nullable=False)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario)
+
+class Coment(Base):
+    __tablename__ = 'coment'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    coment_text = Column(String(250), nullable=False)
+    autor_id = Column(Integer, ForeignKey('follower.id'))
+    follower = relationship(Follower)
+    post_id = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship(Usuario)
+
+
+
+
+
+
+
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    
 
 class Address(Base):
     __tablename__ = 'address'
